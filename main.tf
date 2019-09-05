61d9001b6b99e5e87cde79c5e54c35c9589c83df
tsukaman <tsukaman@mac.com>
2019-09-05 15:43:22 +0900

    add tags to ec2 instance

--- main.tf
+++ main.tf
@@ -12,4 +12,8 @@ resource "aws_instance" "hello-tf-instance" {
   ami           = var.ami
   count         = var.hello_tf_instance_count
   instance_type = var.hello_tf_instance_type
+  tags = map(
+    "owner", "tsukaman",
+    "ttl", "100"
+  )
 }
